0eb90ca1148f7c25b65d6211c2060db177a9f887
Jayden Elk <108333229+SpiritedElk@users.noreply.github.com>
2022-06-29 13:59:25 +0800

    command.py

---
+++ command.py
@@ -0,0 +1,16 @@
+#Somebody isn't working hard? let them feel the rage!
+#change what you want that guy to do down here ↓
+command = "Make your bed!"
+while True:
+  print(command)
+  print("Or... herherher(evil laughter)")
+  a = input("But now I give you a chance! will you "+command, "y or n?")
+  if a = 'y':
+    print("Haha! Now go "+command)
+    break
+  elif a = 'n':
+    print("Disobeying is an awful choice!")
+    print("Again!")
+  else:
+    print("Want to cheat my program? No Way!")
+print("You finally obeyed.")
